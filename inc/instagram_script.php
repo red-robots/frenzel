ab91e362bc2bbe3c5aed32770189b96878ef3998
lisadebona <annalisa.quiamco@gmail.com>
2019-04-02 16:56:28 -0600

    debug instagram

--- inc/instagram_script.php
+++ inc/instagram_script.php
@@ -45,17 +45,20 @@ if($setup) {
                                 
                                 var img = v.images;
                                 var img_src = img.standard_resolution.url;
-                                // var caption = v.caption.text;
-                                // var caption_excerpt = stringTruncate(caption,150);
-                                // var instalink = v.link;
-                                // var content = '<div class="instaCol"><div class="instagram-image-div clear"><a class="instalink" href="'+instalink+'" target="_blank">';
-                                //     content += '<img src="'+img_src+'" alt="" />';
-                                // if(caption) {
-                                //     content += '<span class="caption"><span class="txtwrap">'+caption_excerpt+'</span></span>';
-                                // }
-                                // content += '</a></div></div>';
+                                var caption = '';
+                                if( v.caption.text !=undefined ) {
+                                    caption = v.caption.text;
+                                }
+                                var caption_excerpt = stringTruncate(caption,150);
+                                var instalink = v.link;
+                                var content = '<div class="instaCol"><div class="instagram-image-div clear"><a class="instalink" href="'+instalink+'" target="_blank">';
+                                    content += '<img src="'+img_src+'" alt="" />';
+                                if(caption) {
+                                    content += '<span class="caption"><span class="txtwrap">'+caption_excerpt+'</span></span>';
+                                }
+                                content += '</a></div></div>';
 
-                                // $("#instagram_feeds").append(content);
+                                $("#instagram_feeds").append(content);
                                 console.log(v);
                                 console.log(img);
                             });
